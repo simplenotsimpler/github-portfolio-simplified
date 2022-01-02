ecfb8adaac9c558a79b2faf43099d75a9c81b4fd
SimpleNotSimpler <simplenotsimpler@outlook.com>
2022-01-02 16:26:12 -0500

    Refactor(fetch-work): consolidate via chaining array methods, fixes race condition

--- server/middlewares/fetch-work.js
+++ server/middlewares/fetch-work.js
@@ -23,30 +23,34 @@ const fetchWork = async (req, res, next) => {
 
     res.locals.work=axiosResponse.data;
 
-    // https://amberley.dev/blog/2020-09-07-conditionally-add-to-array-or-obj/
-    // https://www.kevinpeters.net/adding-object-properties-conditionally-with-es-6
-    // const work = res.locals.work.map(item => ({
-    res.locals.work = res.locals.work.map(item => ({
-      ...item,
-      ...(!item.hasOwnProperty('endDate') && {endDate: new Date().toISOString().split('T')[0]})
-    }));
-
-        /* 
-    https://stackoverflow.com/questions/12192491/sort-array-by-iso-8601-date
-    answered Jun 25 '19 at 9:26 icoum
-    myArray.sort(function(a, b) {
-      return new Date(a.date) - new Date(b.date);
-    });      
+    /* 
+      https://amberley.dev/blog/2020-09-07-conditionally-add-to-array-or-obj/
+      https://www.kevinpeters.net/adding-object-properties-conditionally-with-es-6
+    */
+    /* 
+      https://stackoverflow.com/questions/12192491/sort-array-by-iso-8601-date
+      answered Jun 25 '19 at 9:26 icoum
+      myArray.sort(function(a, b) {
+        return new Date(a.date) - new Date(b.date);
+      });      
     
     */
-    res.locals.work.sort((a, b) => new Date(b.endDate) - new Date(a.endDate));
 
-    //add 'Present'
-    res.locals.work = res.locals.work.map(item => ({
-      ...item,
-      ...(item.endDate === currentDate && {endDate: 'Present'})
-    }));
-    
+    res.locals.work = 
+      res.locals.work
+      //if endDate missing, add endDate as currentDate
+      .map(item => ({
+        ...item,
+        ...(!item.hasOwnProperty('endDate') && {endDate: new Date().toISOString().split('T')[0]})
+      }))
+      //sort by endDate
+      .sort((a, b) => new Date(b.endDate) - new Date(a.endDate))
+      //add present
+      .map(item => ({
+        ...item,
+        ...(item.endDate === currentDate && {endDate: 'Present'})
+      })); 
+
     next();
 
   }
