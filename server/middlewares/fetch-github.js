20dfbffb35cb57fbaf4e47df053b12d6d9a1975e
SimpleNotSimpler <simplenotsimpler@outlook.com>
2022-08-03 19:52:14 -0400

    Feature(fetch-github.js): add other counts to query

--- server/middlewares/fetch-github.js
+++ server/middlewares/fetch-github.js
@@ -1,49 +1,54 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const fetchGitHub = async (req, res, next) => {
-
   try {
     const token = process.env.GH_TOKEN;
-    const url = process.env.GH_URL; 
-  
+    const url = process.env.GH_URL;
+
     //MAYBE: remove repositoryTopics? otherwise display
     const ghQueryJSON = `
     {
-      "query": "query {viewer { githubName: name githubLink: url email pinnedItems(first: 10) {repos: nodes { ... on Repository { name description url homepageUrl stargazerCount repositoryTopics(first: 20) { nodes { topic { name } } } languages(first: 20, orderBy: {field: SIZE, direction: DESC}) { languageList:edges {language: node { name } } } } } } } }"
+      "query": "query {viewer { githubName: name githubLink: url email pinnedItems(first: 10) {repos: nodes { ... on Repository { name description url homepageUrl stargazerCount forkCount: forks {totalCount} watchersCount: watchers { totalCount } repositoryTopics(first: 20) { nodes { topic { name } } } languages(first: 20, orderBy: {field: SIZE, direction: DESC}) { languageList:edges {language: node { name } } } } } } } }"
     }
     `;
-  
-   
+
+    
+
     let config = {
-      method: 'post',
+      method: "post",
       url: `${url}`,
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-        'Authorization': `Bearer ${token}`
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${token}`,
       },
       data: ghQueryJSON,
-      timeout: 3000
+      timeout: 3000,
     };
 
     const axiosResponse = await axios(config);
 
-    const {data: {viewer: {githubName, githubLink, email, pinnedItems: {repos}}}}=axiosResponse.data;
-    
-  res.locals.github = {      
+    const {
+      data: {
+        viewer: {
+          githubName,
+          githubLink,
+          email,
+          pinnedItems: { repos },
+        },
+      },
+    } = axiosResponse.data;
+
+    res.locals.github = {
       githubName,
       email,
       githubLink,
-      repos
-    }
+      repos,
+    };
 
     next();
-
-  }
-  catch (error) {
-
+  } catch (error) {
     next(error);
-
   }
-}
+};
 
 module.exports = fetchGitHub;
